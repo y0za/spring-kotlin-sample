5ca3bb110b7c26d23c3535dab27a0b29e34f380f
y0za <yoza.is12s@gmail.com>
2017-08-27 22:52:59 +0900

    Add password column to users table

--- src/main/resources/db/migration/V0001__Create_initial_tables.sql
+++ src/main/resources/db/migration/V0001__Create_initial_tables.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users (
     id BIGINT IDENTITY NOT NULL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
     screen_name VARCHAR(20) NOT NULL,
     display_name VARCHAR(20) NOT NULL,
     created_at TIMESTAMP NOT NULL
